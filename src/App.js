375acd8355ce926ee85c3d6368bfcd0ca3ea9da0
Daud gul <codrdaud@gmail.com>
2022-05-13 17:52:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,155 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import AddIcon from "@mui/icons-material/Add";
+import FeaturedPlayListIcon from "@mui/icons-material/FeaturedPlayList";
+import AppComponent from "./AppComponent";
+import EditIcon from "@mui/icons-material/Edit";
+
+const getItem = () => {
+  let list = localStorage.getItem("lists");
+  if (list) {
+    return JSON.parse(list);
+  } else {
+    return [];
+  }
+};
 
 function App() {
+  const [task, setTaks] = useState("");
+  const [data, setData] = useState(getItem());
+  const [toggleData, setToggleData] = useState(true);
+  const [editText, setEditText] = useState(null);
+  const [addDate, setAddDate] = useState("");
+
+  const changeHendler = (e) => {
+    setTaks(e.target.value);
+    console.log(task);
+  };
+
+  const changeDateHandler = (e) => {
+    setAddDate(e.target.value);
+    console.log(addDate);
+  };
+
+  ////////////////////////////////
+
+  const date = new Date();
+  const fullTime =
+    date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+  const time =
+    date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+  const dateTime = fullTime + " " + time;
+  console.log(dateTime);
+
+  ///////////////////////////////////
+
+  const submitHandler = (a) => {
+    a.preventDefault();
+    if (task.trim().length < 1) {
+      alert("please add value");
+    } else if (task && !toggleData) {
+      setData(
+        data.map((e) => {
+          if (e.id === editText) {
+            return { ...e, name: task };
+          }
+          return e;
+        })
+      );
+      setToggleData(true);
+      setTaks("");
+      setEditText(null);
+    } else {
+      const newData = {
+        id: new Date().getTime().toString(),
+        name: task,
+        today: dateTime,
+      };
+      setData([...data, newData]);
+      setTaks("");
+    }
+  };
+
+  ////////////////////////////////////////
+
+  const deleteItem = (a) => {
+    if (!toggleData) {
+      alert("First Edit your item");
+    } else {
+      const finalData = data.filter((value) => {
+        return a !== value.id;
+      });
+      setData(finalData);
+      setTaks("");
+    }
+  };
+
+  useEffect(() => {
+    localStorage.setItem("lists", JSON.stringify(data));
+  }, [data]);
+
+  //////////////////////////////////////
+
+  const editItem = (id) => {
+    let findItem = data.find((e) => {
+      return e.id === id;
+    });
+    setTaks(findItem.name);
+    setAddDate(findItem.today);
+    setEditText(id);
+    setToggleData(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main">
+        <div className="main-container">
+          <div className="container">
+            <div className="container_item">
+              <h1>
+                add your list here <FeaturedPlayListIcon />
+              </h1>
+              <div className="form-container">
+                <form onSubmit={submitHandler}>
+                  <input
+                    className="input input-item"
+                    type="text"
+                    placeholder="add item"
+                    value={task}
+                    onChange={changeHendler}
+                  />
+
+                  {toggleData ? (
+                    <button className="btn">
+                      <AddIcon
+                        fontSize="large"
+                        color="primary"
+                        aria-label="add"
+                      />
+                    </button>
+                  ) : (
+                    <button className="btn">
+                      <EditIcon
+                        fontSize="large"
+                        color="primary"
+                        aria-label="add"
+                      />
+                    </button>
+                  )}
+                </form>
+              </div>
+            </div>
+          </div>
+          <AppComponent
+            data={data}
+            addDate={addDate}
+            changeDateHandler={changeDateHandler}
+            editItem={editItem}
+            deleteItem={deleteItem}
+          />
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/AppComponent.js
@@ -0,0 +1,52 @@
+import React from "react";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+function AppComponent({
+  data,
+  addDate,
+  changeDateHandler,
+  editItem,
+  deleteItem,
+}) {
+  return (
+    <div className="return-input">
+      {data.map((valu) => {
+        return (
+          <>
+            <div key={valu.id} className="input-data">
+              <div className="input-heading-data">
+                <h3>{valu.name}</h3>
+                <h6
+                  className="date"
+                  value={addDate}
+                  onChange={changeDateHandler}
+                >
+                  {valu.today}:
+                </h6>
+              </div>
+              <div className="input-btn">
+                <button
+                  onClick={() => {
+                    editItem(valu.id);
+                  }}
+                >
+                  <EditIcon />
+                </button>
+                <button
+                  onClick={() => {
+                    deleteItem(valu.id);
+                  }}
+                >
+                  <DeleteIcon />
+                </button>
+              </div>
+            </div>
+          </>
+        );
+      })}
+    </div>
+  );
+}
+
+export default AppComponent;
